ce5f4cedcafaad3c481aa2280e9e581d4d3c3a3a
Sunny <sanyasirao.mopada@gmail.com>
2019-05-04 15:04:18 +0530

    Updated App

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,12 +4,6 @@
 
 import React, { Component } from "react";
 
-// import EagerLoading from "../../components/CodeSplitting/EagerLoading";
-// import LazyLoading from "../../components/CodeSplitting/LazyLoading";
-
-// import WithoutContext from "../../components/Context/WithoutContext";
-// import WithContext from "../../components/Context/WithContext";
-
 import ForwardRef from "../../components/ForwardRef";
 
 class App extends Component {
